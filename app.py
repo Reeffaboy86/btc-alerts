8754d5941ba1412a413d18b696522f240234080c
Reeffaboy86 <clio_maltby@hotmail.co.uk>
2026-09-14 11:24:14 +0100

    Change TARGET_ENTRY from 1.0 to 79950.0
    Updated TARGET_ENTRY value for production use.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Config
 DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1548980198039363586/SbspEcALq9ZqK0LeGqd_D4ZBP2iHOusQEG4BAFWHSk345HC1EMfaSiObMHcbjwY9JXBN"
-TARGET_ENTRY = 1.0  # Set low for testing
+TARGET_ENTRY = 79950.0
 ALERT_COOLDOWN = 900 
 
 last_alert_time = 0
